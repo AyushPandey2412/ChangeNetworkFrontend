68726a60aef5954e0c312f0d0d73baad4ebebf33
AyushPandey2412 <ayushpandey5248@gmail.com>
2025-07-01 19:55:54 +0530

    second

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1722,7 +1722,7 @@ const TaskList = ({ refresh, searchText }) => {
                   value={statusFilter}
                   onChange={(e) => setStatusFilter(e.target.value)}
                 >
-                  <option value="">All Statuses</option>
+                  <option value="">All Status</option>
                   <option>To Do</option>
                   <option>In Progress</option>
                   <option>Done</option>
